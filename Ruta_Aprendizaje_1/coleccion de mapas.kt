df648709f6ca61a64ce3bc08319a44e1e3725851
cniki2651-arch <cniki2651@gmail.com>
2026-09-13 01:12:18 -0500

    Add files via upload

---
+++ Ruta_Aprendizaje_1/coleccion de mapas.kt
@@ -0,0 +1,35 @@
+//coleccion de mapas
+fun main() {
+    // Crear un mapa mutable con valores iniciales
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+
+    
+    println(solarSystem.size) 
+
+   
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size) 
+
+    
+    println(solarSystem["Pluto"]) 
+
+    
+    println(solarSystem.get("Theia")) 
+
+    
+    solarSystem.remove("Pluto")
+    println(solarSystem.size) 
+
+   
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"]) 
+}
